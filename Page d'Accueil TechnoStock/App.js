ec3f5409ab98c4c08aaef7dc6a36744fe374de34
Victor de Spirlet <115162928+VicdSpt@users.noreply.github.com>
2024-03-06 22:24:54 +0100

    Add files via upload

---
+++ Page d'Accueil TechnoStock/App.js
@@ -0,0 +1,17 @@
+// const input_fields = document.querySelectorAll('input');
+
+// for (let i = 0; i < input_fields.length; i++){
+
+//     let field = input_fields[i];
+
+//     field.addEventListener('input', function(e){
+
+//         if(e.target.value != ""){
+//             e.target.parentNode.classList.add('animation');
+//         } else if (e.target.value == ""){
+//             e.target.parentNode.classList.remove('animation')
+//         }
+
+//     })
+
+// }
